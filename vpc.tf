6f5ae947c42622e23426fbefbcb5fa1c086d9248
Ryan Brooks <ryan@slatehorse.com>
2018-03-19 16:56:44 +0000

    Enable NAT gateway for private network traffic

--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,9 @@ module "vpc" {
   cidr = "${var.vpc_cidr}"
 
   enable_dns_hostnames = true
-  
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
   azs             = "${var.availability_zones}"
   private_subnets = "${var.private_subnets}"
   public_subnets  = "${var.public_subnets}"
